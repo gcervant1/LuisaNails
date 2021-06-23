c871b6dd40de83c9e8b213b1324bf7c752311fdf
Gonzo  Cervantelli <gonzo@Gonzos-MacBook-Pro.local>
2021-06-23 16:18:24 -0500

    addition od site

---
+++ src/Components/Contact.js
@@ -0,0 +1,151 @@
+import React, { Component } from "react";
+import { Fade, Slide } from "react-reveal";
+
+class Contact extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const name = this.props.data.name;
+    const street = this.props.data.address.street;
+    const city = this.props.data.address.city;
+    const state = this.props.data.address.state;
+    const zip = this.props.data.address.zip;
+    const phone = this.props.data.phone;
+    const message = this.props.data.contactmessage;
+
+    return (
+      <section id="contact">
+        <Fade bottom duration={1000}>
+          <div className="row section-head">
+            <div className="two columns header-col">
+              <h1>
+                <span>Contactanos.</span>
+              </h1>
+            </div>
+
+            <div className="ten columns">
+              <p className="lead">{message}</p>
+            </div>
+          </div>
+        </Fade>
+
+        <div className="row">
+          <Slide left duration={1000}>
+            <div className="eight columns">
+              <form action="" method="post" id="contactForm" name="contactForm">
+                <fieldset>
+                  <div>
+                    <label htmlFor="contactName">
+                      Nombre <span className="required">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      defaultValue=""
+                      size="35"
+                      id="contactName"
+                      name="contactName"
+                      onChange={this.handleChange}
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="contactEmail">
+                      Email <span className="required">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      defaultValue=""
+                      size="35"
+                      id="contactEmail"
+                      name="contactEmail"
+                      onChange={this.handleChange}
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="contactSubject">Motivo</label>
+                    <input
+                      type="text"
+                      defaultValue=""
+                      size="35"
+                      id="contactSubject"
+                      name="contactSubject"
+                      onChange={this.handleChange}
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="contactMessage">
+                      Mensaje <span className="required">*</span>
+                    </label>
+                    <textarea
+                      cols="50"
+                      rows="15"
+                      id="contactMessage"
+                      name="contactMessage"
+                    ></textarea>
+                  </div>
+
+                  <div>
+                    <button className="submit">Enviar</button>
+                    <span id="image-loader">
+                      <img alt="" src="images/loader.gif" />
+                    </span>
+                  </div>
+                </fieldset>
+              </form>
+
+              <div id="message-warning"> Error boy</div>
+              <div id="message-success">
+                <i className="fa fa-check"></i>Tu mensaje fue enviado, Gracias!
+                <br />
+              </div>
+            </div>
+          </Slide>
+
+          <Slide right duration={1000}>
+            <aside className="four columns footer-widgets">
+              <div className="widget widget_contact">
+                <h4>Direccion y Telefono</h4>
+                <p className="address">
+                  {name}
+                  <br />
+                  {street} <br />
+                  {city}, {state} {zip}
+                  <br />
+                  <span>{phone}</span>
+                </p>
+              </div>
+
+              <div className="widget widget_tweets">
+                <h4 className="widget-title">Ultimas Tendencias</h4>
+                <ul id="twitter">
+                  <li>
+                    <span>
+                      Las uñas que son tendencia esta primavera 2021, Los colores pastel, 'nail arts' discretos y la clásica manicura francesa.
+                      <a href="https://www.revistavanityfair.es/lujo/belleza/articulos/tendencias-manicura-primavera-verano-2021/49841"> ver nuevas vanity fair</a>
+                    </span>
+                    <b>
+                      <a href="./">Hace 1 dia</a>
+                    </b>
+                  </li>
+                  <li>
+                    <span>
+                      Los colores de uñas de 2021 o por qué las manicuras nos obsesionan más que nunca
+                      <a href="https://www.vogue.es/belleza/articulos/colores-unas-manicuras-2021-tendencias-francesa-fucsia-sin-pintar"> ver en Vogue</a>
+                    </span>
+                    <b>
+                      <a href="./">Hace 2 dias</a>
+                    </b>
+                  </li>
+                </ul>
+              </div>
+            </aside>
+          </Slide>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Contact;
